1832b0713adad1eb028714dcdb338d4e17d41c39
patrick-replogle <patrickr1138@gmail.com>
2020-02-14 15:12:20 -0800

    updated code

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -25,8 +25,8 @@ router.post("/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        //req.session.user = user
-        req.session.loggedin = true;
+        req.session.user = user;
+        //req.session.loggedin = true;
 
         res.status(200).json({
           message: `Welcome ${user.username}!`
