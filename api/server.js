94cddede889c38c904e3722b9abc24fde09f3bde
patrick-replogle <patrickr1138@gmail.com>
2020-02-13 20:40:01 -0800

    completed guided project

--- api/server.js
+++ api/server.js
@@ -1,21 +1,46 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session");
+const knexSessionStore = require("connect-session-knex")(session);
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const dbConfig = require("../database/dbConfig.js");
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
 
 const server = express();
+
+const sessionConfig = {
+  name: "monkey",
+  secret: "keep it secret, keep it safe!",
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    secure: false, // use true in production for https
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: false,
+
+  store: new knexSessionStore({
+    knex: dbConfig,
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
+};
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+//const sessionMiddleware = session(sessionConfig)
+server.use(session(sessionConfig));
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
-server.get('/', (req, res) => {
-  res.json({ api: 'up' });
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
 });
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
-
-const Users = require('../users/users-model.js');
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const restricted = require("./restricted-middleware");
+const Users = require("../users/users-model.js");
 
 // for endpoints beginning with /api/auth
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
   user.password = hash;
@@ -18,23 +18,50 @@ router.post('/register', (req, res) => {
     });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
+        req.session.skippy = "peanutbutter";
         res.status(200).json({
-          message: `Welcome ${user.username}!`,
+          message: `Welcome ${user.username}!`
         });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Invalid Credentials" });
       }
     })
     .catch(error => {
       res.status(500).json(error);
     });
+});
+
+router.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.send(
+          "you can check out any time you like, but you can never leave..."
+        );
+      } else {
+        res.send("so long, thanks for all the fish.");
+      }
+    });
+  } else {
+    res.end();
+  }
+});
+
+// maurer's random protected test route
+router.get("/protected", restricted, async (req, res, next) => {
+  try {
+    res.json({ message: "you are authorized" });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
